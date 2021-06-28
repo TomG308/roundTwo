f74f283d53b702542f389ee85e905ce1ad96fd07
TomG308 <thomas@vindicoded.com>
2021-06-28 01:47:39 -0600

    finished adding getById update and delete rotues

--- backend/routes/supporters.js
+++ backend/routes/supporters.js
@@ -8,19 +8,51 @@ router.route('/').get((req, res) => {
 })
 
 router.route('/add').post((req, res) => {
-    const name = req.body.name
+    const firstName = req.body.firstName
+    const lastName = req.body.lastName
     const email = req.body.email
-    const number = req.body.number
+    const areaCode = req.body.areaCode
+    const phoneNumber = req.body.phoneNumber
 
     const newSupporters = new Supporters({
-        name,
+        firstName,
+        lastName,
         email,
-        number,
+        areaCode,
+        phoneNumber,
     })
 
     newSupporters.save()
     .then(() => res.json('supporter added!!!'))
     .catch(err => res.status(400).json('Error: ' + err))
+})
+
+router.route('/:id').get((req, res) => {
+    Supporters.findById(req.params.id)
+        .then(supporters => res.json(supporters))
+        .catch(err => res.status(400).json('Error:' + err))
+})
+
+router.route('/:id').delete((req, res) => {
+    Supporters.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Supporter deleted.'))
+    .catch(err => res.status(400).json('Error:' + err))
+})
+
+router.route('/update/:id').post((req, res) => {
+    Supporters.findById(req.params.id)
+        .then(supporters => {
+            supporters.firstName = req.body.firstName
+            supporters.lastName = req.body.lastName
+            supporters.email = req.body.email
+            supporters.areaCode = req.body.areaCode
+            supporters.phoneNumber = req.body.phoneNumber
+
+            supporters.save()
+                .then(() => res.json('Supporter updated')   )
+                .catch(err => res.status(400).json('Error: ' + err))
+        })
+        .catch(err => res.status(400).json('Error: ' + err))
 })
 
 module.exports = router
